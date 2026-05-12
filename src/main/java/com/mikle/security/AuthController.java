03c4e838611ebec1a9d3249b99253d9bbe9a38ee
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-12 03:59:11 +0300

    feat: add mapping request for create new user

--- src/main/java/com/mikle/security/AuthController.java
+++ src/main/java/com/mikle/security/AuthController.java
@@ -1,5 +1,14 @@
 package com.mikle.security;
 
+import com.mikle.service.UserService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @Getter
+    @Setter
+    private static class AuthRequest {
+        private String username;
+        private String password;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody AuthRequest request) {
+        userService.register(request.getUsername(), request.getPassword());
+        return ResponseEntity.ok().build();
+    }
+
+
 }
