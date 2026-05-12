ecc9d06155fe5570ef90aef3881228f98c731f43
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-12 03:31:39 +0300

    feat: init UrlService and write basic functional for add new short url

--- src/main/java/com/mikle/service/UrlService.java
+++ src/main/java/com/mikle/service/UrlService.java
@@ -1,17 +1,59 @@
 package com.mikle.service;
 
+import com.mikle.model.Url;
+import com.mikle.model.User;
 import com.mikle.repository.UrlRepository;
+import com.mikle.repository.UserRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Getter
 @Setter
 @Service
 public class UrlService {
-    private final UrlRepository repo;
+    @Autowired
+    private final UrlRepository urlRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public UrlService(UrlRepository repo) {
-        this.repo = repo;
+        this.urlRepository = repo;
     }
+
+    @Transactional
+    public Url addUrl(String url) {
+        User user = getCurrentUser();
+
+        System.out.println("Principal: " + user);
+        Url shortUrl = new Url();
+        shortUrl.setUser(user);
+
+        return urlRepository.save(shortUrl);
+    }
+
+    private User getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        switch (principal) {
+            case User user -> {
+                return user;
+            }
+            case org.springframework.security.core.userdetails.User user -> {
+                String username = user.getUsername();
+                return userRepository.findByUsername(username)
+                        .orElseThrow(() -> new RuntimeException("User not found: " + username));
+            }
+            case String s -> {
+                return userRepository.findByUsername(s)
+                        .orElseThrow(() -> new RuntimeException("User not found: " + principal));
+            }
+            default -> throw new RuntimeException("Unknown principal type: " + principal.getClass().getName());
+        }
+    }
+
 }
