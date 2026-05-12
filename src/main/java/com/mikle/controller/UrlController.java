597a251a15d3dcd5b3796a823fd49c4c4b77cb00
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-12 03:40:02 +0300

    feat: add mapping request for add new url

--- src/main/java/com/mikle/controller/UrlController.java
+++ src/main/java/com/mikle/controller/UrlController.java
@@ -1,11 +1,40 @@
 package com.mikle.controller;
 
 
+import com.mikle.model.Url;
+import com.mikle.service.UrlService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 @RequestMapping("/api/url")
 public class UrlController {
+    @Autowired
+    private UrlService urlService;
+
+    @Getter
+    static private class UrlRequest {
+        private String url;
+    }
+
+    @PostMapping
+    public ResponseEntity<Url> addUrl(@RequestBody UrlRequest request) {
+        try {
+            Url shortUrl = urlService.addUrl(request.getUrl());
+            return ResponseEntity.ok(shortUrl);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
 }
+
