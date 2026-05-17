922bd42997e2db2c589c89aab090d45cb52c72dd
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-17 05:12:22 +0300

    fix: made User model only Entity

--- src/main/java/com/mikle/model/User.java
+++ src/main/java/com/mikle/model/User.java
@@ -1,19 +1,14 @@
 package com.mikle.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,13 +19,4 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public String getPassword() { return password; }
-    @Override
-    public String getUsername() { return username; }
 }
