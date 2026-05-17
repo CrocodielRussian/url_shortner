728a1c1265aa99f49bb06f8075252dd5da737339
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-17 05:08:03 +0300

    feat: rewrite controller mapping requets  for login and register user

---
+++ src/main/java/com/mikle/security/AuthController.java
@@ -0,0 +1,31 @@
+package com.mikle.security;
+
+import com.mikle.dto.AuthRequest;
+import com.mikle.dto.AuthResponse;
+import com.mikle.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(authService.register(request));
+    }
+}
