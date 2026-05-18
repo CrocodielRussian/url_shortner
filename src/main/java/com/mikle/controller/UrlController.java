3ec9663456f2371d20b25bbc84cb3f2122ea5e44
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-18 15:56:09 +0300

    fix: rewrite addUrl to add

--- src/main/java/com/mikle/controller/UrlController.java
+++ src/main/java/com/mikle/controller/UrlController.java
@@ -1,9 +1,9 @@
 package com.mikle.controller;
 
 
+import com.mikle.dto.UrlRequest;
 import com.mikle.model.Url;
 import com.mikle.service.UrlService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,23 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-
 @RestController
 @RequestMapping("/api/url")
 public class UrlController {
     @Autowired
     private UrlService urlService;
 
-    @Getter
-    static private class UrlRequest {
-        private String url;
-    }
 
     @PostMapping
-    public ResponseEntity<Url> addUrl(@RequestBody UrlRequest request) {
+    public ResponseEntity<Url> add(@RequestBody UrlRequest request) {
         try {
-            Url shortUrl = urlService.addUrl(request.getUrl());
+            Url shortUrl = urlService.addUrl(request.getShortUrl());
             return ResponseEntity.ok(shortUrl);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(null);
